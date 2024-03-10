bf3a7659d939e0d118e39a8bf2db573404b0ba8f
Rajat Sancheti <sancraja@amazon.com>
2024-03-10 16:26:22 +0530

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
-import Card from './Card';
+import { Drawer, DrawerContent, DrawerAppContent, DrawerHeader, DrawerSubtitle, DrawerTitle } from '@rmwc/drawer';
+import { List, ListItem } from '@rmwc/list';
+import { TopAppBar, TopAppBarRow, TopAppBarSection, TopAppBarNavigationIcon, TopAppBarTitle, TopAppBarActionItem, TopAppBarFixedAdjust } from '@rmwc/top-app-bar';
+import '@material/drawer/dist/mdc.drawer.css';
+import '@rmwc/list/styles';
+import '@rmwc/top-app-bar/styles';
+
+function Example() {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <>
+      <div style={{ overflow: 'hidden', position: 'relative' }}>
+        <Drawer dismissible open={open}>
+          <DrawerHeader>
+            <DrawerTitle>DrawerHeader</DrawerTitle>
+            <DrawerSubtitle>Subtitle</DrawerSubtitle>
+          </DrawerHeader>
+          <DrawerContent>
+            <List>
+              <ListItem>Cookies</ListItem>
+              <ListItem>Pizza</ListItem>
+              <ListItem>Icecream</ListItem>
+            </List>
+          </DrawerContent>
+        </Drawer>
+
+        {/* Optional DrawerAppContent */}
+        <DrawerAppContent style={{ minHeight: '15rem', padding: '1rem' }}>
+          DrawerAppContent is an optional component that will resize content
+          when the dismissible drawer is open and closed. It must be placed
+          directly after the Drawer component.
+        </DrawerAppContent>
+      </div>
+
+      <div style={{ textAlign: 'center' }}>
+        <button onClick={() => setOpen(!open)}>
+          Toggle Dismissible
+        </button>
+      </div>
+    </>
+  );
+}
 
 function App() {
+  // const [isDrawerOpen, setDrawerOpen] = React.useState(false);
+  
+  // console.log('nene', isDrawerOpen);
+
+  // useEffect(() => {
+  //   console.log('new value is', isDrawerOpen);
+  // }, [isDrawerOpen])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Card />
-      </header>
+      {/* {renderTopbar(toggleDrawerState)} */}
+
+      {/* <>
+        <TopAppBar>
+          <TopAppBarRow>
+            <TopAppBarSection alignStart>
+              <TopAppBarNavigationIcon icon="menu" onClick={() => setDrawerOpen(prev => !prev)} />
+              <TopAppBarTitle>All Features</TopAppBarTitle>
+            </TopAppBarSection>
+            <TopAppBarSection alignEnd>
+              <TopAppBarActionItem icon="favorite" onClick={() => setDrawerOpen(prev => !isDrawerOpen)} />
+              <TopAppBarActionItem icon="star" />
+              <TopAppBarActionItem icon="mood" />
+            </TopAppBarSection>
+          </TopAppBarRow>
+        </TopAppBar>
+        <TopAppBarFixedAdjust />
+      </> */}
+
+      <Example />
+
+      {/* <div className="main-content">
+        <Drawer onOpen={() => console.log('opening')} onClose={() => console.log('closing')} dismissible={true} open={isDrawerOpen}>
+          
+          <DrawerContent>
+            <List>
+              <ListItem>Cookies</ListItem>
+              <ListItem>Pizza</ListItem>
+              <ListItem>Icecream</ListItem>
+            </List>
+          </DrawerContent>
+        </Drawer>
+
+        <DrawerAppContent
+            style={{ minHeight: '16rem', padding: '4rem' }}
+          >
+            DrawerAppContent is an optional component that will resize
+            content when the dismissible drawer is open and closed. It
+            must be placed directly after the Drawer component.
+          </DrawerAppContent>
+
+      </div> */}
+      
     </div>
   );
 }
