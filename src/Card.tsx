1535922b0663db3c5a3d30c3f6e5be885c46fd4f
Rajat Sancheti <sancraja@amazon.com>
2024-03-10 20:25:56 +0530

    Add drawer and header

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect } from 'react';
 
 export default () => {
-  const fetchText = () => {
-    const text = fetch("test/NY/p1.json").then(res => {
-      console.log("inside", res.json())
-    })
-    console.log('blblb', text)
-  }
+  // const fetchText = () => {
+  //   const text = fetch("test/NY/p1.json").then(res => {
+  //     console.log("inside", res.json())
+  //   })
+  //   console.log('blblb', text)
+  // }
 
-  useEffect(() => {
-    fetchText();
-  }, [])
+  // useEffect(() => {
+  //   fetchText();
+  // }, [])
 
   return (
     <div>
